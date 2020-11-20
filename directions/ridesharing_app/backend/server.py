dcd65bcfd0f35c6d08ac3be69dd049ee75689552
Carlos Aguilar Miguel <caguilar@parkifast.com>
2020-11-20 09:26:35 +0000

    Review and comment backend server python script

--- directions/ridesharing_app/backend/server.py
+++ directions/ridesharing_app/backend/server.py
@@ -2,15 +2,21 @@ import http.server
 import socketserver
 import os, json
 
+# Simple HTTP Server to simulate a Backend API 
+# to response matrix request from a Frontend APP.
+
 PORT = 8000
 
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        # Read allocations generated
         data = open(os.path.join("..", "database", "allocations.json"), "r").read()
+        # Create the response
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
+        # Send response to the client
         self.wfile.write(data.encode())
 
 with socketserver.TCPServer(("", PORT), RequestHandler) as httpd:
